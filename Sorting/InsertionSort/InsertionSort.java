297c5de333d2c6ac661ca925c8998315b2e15916
lulafar <77245959+lulafar@users.noreply.github.com>
2021-02-15 10:40:49 +0100

    Update InsertionSort.java

--- Sorting/InsertionSort/InsertionSort.java
+++ Sorting/InsertionSort/InsertionSort.java
@@ -1,1 +1,40 @@
-
+// Programa Java para implementar Ordenación de Inserción
+class InsertionSort { 
+    /*Función que implementa el algoritmo
+    void sort(int arr[]) 
+    { 
+        int n = arr.length; 
+        for (int i = 1; i < n; ++i) { 
+            int key = arr[i]; 
+            int j = i - 1; 
+  
+            /* Mover los elementos que son mayores que el que estamos comparando a una posición más a la derecha de su actual posición */
+            while (j >= 0 && arr[j] > key) { 
+                arr[j + 1] = arr[j]; 
+                j = j - 1; 
+            } 
+            arr[j + 1] = key; 
+        } 
+    } 
+  
+    /* Función que pinta el array ordenado*/
+    static void printArray(int arr[]) 
+    { 
+        int n = arr.length; 
+        for (int i = 0; i < n; ++i) 
+            System.out.print(arr[i] + " "); 
+  
+        System.out.println(); 
+    } 
+  
+    // Driver método
+    public static void main(String args[]) 
+    { 
+        int arr[] = { 12, 11, 13, 5, 6 }; 
+  
+        InsertionSort ob = new InsertionSort(); 
+        ob.sort(arr); 
+  
+        printArray(arr); 
+    } 
+}
