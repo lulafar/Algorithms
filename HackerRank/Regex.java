a1d89cbfbee0624c2dd5331b1f6e0a9fd87e9a13
lulafar <77245959+lulafar@users.noreply.github.com>
2021-02-24 15:12:19 +0100

    Update Regex.java

--- HackerRank/Regex.java
+++ HackerRank/Regex.java
@@ -2,14 +2,14 @@ String zeroTo255 = "(\\d{1,2}|(0|1)\\d{2}|2[0-4]\\d|25[0-5])";
 public String pattern = zeroTo255 + "\\." + zeroTo255 + "\\." + zeroTo255 + "\\." + zeroTo255;
 
 /*
-1) \\d{1,2} catches any one or two digit number
+1) \\d{1,2} acepra números tanto de 1 dígito como 2 (0-99)
 
-2) (0|1)\\d{2} catches any three digit number starting with 0 or 1.
+2) (0|1)\\d{2} acepta números de 3 dígitos, que empiecen o por o por 2 (100-299)
 
-3) 2[0-4]\\d catches numbers between 200 and 249.
+3) 2[0-4]\\d acepta número de 3 cifras empezando con 2 y como segundo dígito del 0 a 4 (200-249)
 
-4) 25[0-5] catches numbers between 250 and 255.
+4) 25[0-5] número de tres cifras que empiece por 25 y el último dígito entre 0 y 5 (250-255)
 
-Note that \d represents digits in regular expressions, same as [0-9]
+\d representa dígitos en una expresión regular, es lo mismo que decir [0-9]
+*/
 
-*\
